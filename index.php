eb11deab756ff55d2de52f5b111dbd0248806947
rymnd-crrd <161231297+rymnd-crrd@users.noreply.github.com>
2024-06-05 00:33:28 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<html lang="en">
+   <head>
+    <body>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"> 
+  <link rel="stylesheet" href="index.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Font+Name&display=swap">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=DM+Sans&display=swap">
+
+
+  <div class="header"> 
+   <h1>WELCOME TO</h1>
+   <h2>My Teapots</h2>
+   <h3>Milktea & Coffee</h3>
+    
+   <a href="home.php" class="button">VIEW MORE</a> 
+    <div class="overlay"></div>
+    </div>  
+   <script src="try.js"></script>
+   
+ </body>
+</html>
